4003ee37dd2bcd4d61e6c703b429ee227079c986
Kartik Pahwa <kartikpahwa02@gmail.com>
2026-06-14 01:33:29 +0530

    Update 16-3Sum-Closest.cpp

--- 16-3Sum-Closest.cpp
+++ 16-3Sum-Closest.cpp
@@ -1,28 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    int threeSumClosest(vector<int>& nums, int target) {
+        int res = 0;
+        int min_diff = INT_MAX; 
         int n = nums.size();
-        vector<vector<int>> res;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int j=i+1,k=n-1;
-            int sum=0;
-            while(j<k){
-                sum=nums[j]+nums[k];    
-            if(sum==(-nums[i])){
-                 res.push_back({nums[i],nums[j],nums[k]});
-                 j++;
-                 k--;
-                 while(j<k && nums[j]==nums[j-1]) j++;
-                 while(j<k && nums[k]==nums[k+1]) k--;
-                 
-            }
-            else if(sum>(-nums[i]))
-                k--;
-            else
-                j++;
+        
+        sort(nums.begin(), nums.end());
+        
+        for(int i = 0; i < n; i++){
+            int j = i + 1;
+            int k = n - 1;
             
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                int diff = abs(sum - target);
+                
+                if(diff < min_diff){
+                    min_diff = diff;
+                    res = sum;
+                }
+                
+                if(sum == target){
+                    return sum; 
+                }
+                else if(sum < target){
+                    j++; 
+                }
+                else{
+                    k--; 
+                }
             }
         }
         return res;
